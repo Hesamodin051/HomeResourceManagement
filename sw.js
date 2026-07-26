e3f69288e2b8b22d97cdb8570a0f64099db9111e
HesamodinKadkhoda <165418567+Hesamodin051@users.noreply.github.com>
2026-07-26 22:04:16 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,12 @@
 // sw.js
-const CACHE_NAME = 'tadbir-v1';
-const STATIC_URLS = ['/', '/index.html', '/dashboard.html', '/style.css', '/app.js', '/manifest.json'];
-
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_URLS))
-    );
+self.addEventListener('install', function(event) {
+  event.waitUntil(self.skipWaiting());
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => response || fetch(event.request))
-    );
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(fetch(event.request));
 });
