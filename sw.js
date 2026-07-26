3b5fd876c0abb5e7bd0936a643c1f1d3244f77fa
HesamodinKadkhoda <165418567+Hesamodin051@users.noreply.github.com>
2026-07-27 01:16:33 +0330

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,14 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(self.skipWaiting());
+});
+self.addEventListener('activate', function(event) {
+    event.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', function(event) {
+    event.respondWith(fetch(event.request).catch(function() {
+        return new Response('⛔ صفحه در حالت آفلاین در دسترس نیست.', {
+            status: 404,
+            statusText: 'Not Found'
+        });
+    }));
+});
